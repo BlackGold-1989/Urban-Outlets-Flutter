844c58ad9ea32eb1cea59f3e6977c94e82fb30ac
BlackGold-1989 <bgold1118@gmail.com>
2021-04-09 06:34:36 -0400

    init project

---
+++ lib/themes/color_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffb000ff);
+
+const pinkColor = Color(0xffb000ff);
+const blackColor = Color(0xff0e0017);
+const greenColor = Color(0xff00D05E);
+const blueColor = Color(0xff17CAFF);
+const redColor = Color(0xffFF4545);
